4ee81b2b43b25c61dda22fa24502ae04a53a9d99
drew <drewcodes@gmail.com>
2016-08-12 15:27:09 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,31 @@
 $(document).ready(function () {
     
     //GSAP
+    TweenMax.to( '.spinner', 5, {x:0, rotation:360, repeat:100});
+    TweenMax.from( '#msg1', 1.5, {opacity:0});
+    TweenMax.from( '#msg2', 3, {opacity:0, delay: 1});
+    TweenMax.from( '#msg3', 1, {x:-500, opacity:0, scale:1, delay:2});
+    TweenMax.staggerFrom('.dot', 0.5, {opacity:0, y:150, delay: 0.5}, 0.3);
     
-    TweenMax.from( '#msg1', 1, {x:-500});
-    TweenMax.from( '#msg2', 1, {opacity:0, scale:0, ease:Bounce.easeOut, delay: 1});
-    TweenMax.from( '#msg3', 1, {x:0, opacity:0, delay:2});
-    TweenMax.staggerFrom('.dot', 0.5, {opacity:0, y:200, delay: 0.5}, 0.3);
+      //TweenMax.to('.dot', 0.5, {opacity:0, delay:2});
     
+    //scrollmagic
+    var controller = new ScrollMagic.Controller();
+
+	// portfolio scene
+	var portfolio = new ScrollMagic.Scene({
+		triggerElement: '.portfolio'
+	})
+	.setClassToggle('.portfolio', 'fade-in')
+	.addTo(controller);
+    
+    
+    //about scene
+    var about = new ScrollMagic.Scene({
+		triggerElement: '.about'
+	})
+	.setClassToggle('.about', 'fade-in')
+	.addTo(controller);
     
     //smooth-scroll
      $('a[href^="#"]').on('click', function (e) {
