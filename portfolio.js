93207bbaeb6101cef25e33c1409991679a836ffa
drew <drewcodes@gmail.com>
2016-08-12 10:50:46 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    
+    //GSAP
+    
+    TweenMax.from( '#msg1', 1, {x:-500});
+    TweenMax.from( '#msg2', 1, {opacity:0, scale:0, ease:Bounce.easeOut, delay: 1});
+    TweenMax.from( '#msg3', 1, {x:0, opacity:0, delay:2});
+    TweenMax.staggerFrom('.dot', 0.5, {opacity:0, y:200, delay: 0.5}, 0.3);
+    
+    
+    //smooth-scroll
      $('a[href^="#"]').on('click', function (e) {
          e.preventDefault();
 
