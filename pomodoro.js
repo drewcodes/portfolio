c69809e9a0e4e10022ac44e0bd7ab9298144a846
drew <drewcodes@gmail.com>
2016-08-29 10:25:04 -0400

    Create pomodoro.js

---
+++ pomodoro.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+    //Hide reset buttons until is up.
+    $('.reset').hide();
+    // vars for actual time
+    var time = parseInt($('#num').html());
+    var breakTime = parseInt($('#breakNum').html());
+    //Start button
+    $('#start').click(function () {
+        var countdown = setInterval(seconds, 1000);
+        time *= 60;
+
+        function seconds() {
+            // Hide buttons
+            $('#start, #pomodoro-minus-five, #pomodoro-plus-five, #break-minus-five, #break-plus-five, #breakNum, #pomodoro-time, #break-time').hide();
+            // Enlarge Timer
+            $('#num').addClass('enlarge');
+            $('#active-time').show();
+            $('#active-time').html('Time Left:');
+            time -= 1;
+            if (time === 0) {
+                alert("Time's up!");
+                clearInterval(countdown);
+                var startBreak = setInterval(breakTimer, 1000);
+                $('#num').hide();
+            }
+            // Seconds to Mins Conversion
+            if (time % 60 >= 10) {
+                $('#num').html(Math.floor(time / 60) + ":" + time % 60);
+            }
+            else {
+                // need this statement to correctly display numbers
+                $('#num').html(Math.floor(time / 60) + ":" + '0' + time % 60);
+            }
+            //Break Timer
+            function breakTimer() {
+                $('#active-time').show();
+                $('#active-time').html('Break time left:');
+                $('#breakNum').show();
+                breakTime -= 1;
+                if (breakTime === 0) {
+                    clearInterval(startBreak);
+                    $('#reset-pomo').show();
+                    $('#breakNum, #active-time').hide();
+                }
+                $('#breakNum').html(breakTime);
+            }
+        }
+    });
+    //Reset Button
+    $('#reset-pomo').click(function () {
+        time = 25;
+        breakTime = 5;
+        $('#num').html(time);
+        $('#breakNum').html(breakTime);
+        $('#pomodoro-time, #break-time, #num, #breakNum, #pomodoro-minus-five, #pomodoro-plus-five, #break-minus-five, #break-plus-five, #start').show();
+        $('#reset-pomo').hide();
+    });
+    // Pomodoro Clock Session Increment & Decrement Buttons
+    $('#pomodoro-minus-five').click(function () {
+        if (time > 5) {
+            time -= 5;
+            $('#num').html(time);
+        }
+    });
+    $('#pomodoro-plus-five').click(function () {
+        if (time >= 5) {
+            time += 5;
+            $('#num').html(time);
+        }
+    });
+    // Break Buttons
+    $('#break-minus-five').click(function () {
+        if (breakTime > 5) {
+            breakTime -= 5;
+            $('#breakNum').html(breakTime);
+        }
+    });
+    $('#break-plus-five').click(function () {
+        breakTime += 5;
+        $('#breakNum').html(breakTime);
+        console.log(breakTime);
+    });
+});
