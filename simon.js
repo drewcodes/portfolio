e6b64afc1ce228e8d4a4c8cb2ba7fb84453091e4
drew <drewcodes@gmail.com>
2016-12-14 07:55:38 -0500

    Update simon.js

--- simon.js
+++ simon.js
@@ -6,23 +6,22 @@ $(document).ready(function () {
     var audio3 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
     var audio4 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
     var audioBuzzer = new Audio('https://s3-us-west-2.amazonaws.com/guylemon/Buzzer.mp3');
-    //Win or Lost
-    var win = false;
-    //Arr for player to input their moves.
+    // Win or Lose
+    var win = false;   // Array for player to input their moves.
     var player = [];
-    //comp seq used to animate, play game and compare player seq against
+    // Computer sequence used to animate, play game and compare player seq against
     var compMoves = [];
-    //holds arr interval position for var computer, pushing the one move at a time into compMoves;
+    // Holds array interval position for var computer, pushing the one move at a time into compMoves;
     var compPosition = 0;
-    //Arr for random generated sequence to push into
+    // Array for random generated sequence to push into
     var computer = [];
-    //COUNTER
+    // Counter / Current Round
     var counter = 0;
     // Strict Toggle
     var strict = false;
-    //Game Speed Time
+    // Game Speed Time
     var speed = 1000;
-    //Color Light Speed
+    // Color Light Speed
     var light = 500;
     // Power Button
     var power = false;
@@ -44,6 +43,7 @@ $(document).ready(function () {
         computer = [];
         compMoves = [];
         player = [];
+        light = 500;
         $('.counter').html("--");
         strict = false;
         $('.strict-light').css('background', "black");
@@ -61,6 +61,7 @@ $(document).ready(function () {
         generate20Moves();
         compMoves = [];
         player = [];
+        light = 500;
         counter = 0;
         $('.counter').html(counter);
         $('.game-button').addClass('active');
@@ -72,7 +73,7 @@ $(document).ready(function () {
         }, 1500);
     }
     // Strict Button
-    // when true, if wrong input => call strict() to reset everything
+       // When strict is on, if wrong input, call strict() to reset everything
     $('.strict-button').click(function () {
         if (power === true) {
             if (strict == false) {
@@ -99,10 +100,8 @@ $(document).ready(function () {
     }
     // Pushes random move into computer sequence array
     function compMove() {
-        if (compPosition <= 19) {
-            //pushes just ONE value from computer arr into compMoves
-            compMoves.push(computer[compPosition]);
-            //increments to the next interval when function is called again
+        if (compPosition <= 19) {       // Pushes just ONE value from computer arr into compMoves
+            compMoves.push(computer[compPosition]);       // Increments to the next interval position  when function is called again
             compPosition += 1;
         }
     }
@@ -177,8 +176,12 @@ $(document).ready(function () {
                 }, i * speed);
             });
         }
+        // Prevents color buttons from being active while animation plays
+        setTimeout(function () {
+            $('.game-button').addClass('active');
+        }, (compMoves.length) * speed);
     }
-    // Config for player sequence input. Runs check logic.
+    // Config for player sequence input. Runs check() logic.
     $('.red').click(function () {
         player.push(1);
         red();
@@ -207,6 +210,7 @@ $(document).ready(function () {
             if (player.length === compMoves.length) {
                 // If true, player passes round
                 setTimeout(function () {
+                    $('.game-button').removeClass('active');
                     player = [];
                     if (win === false) {
                         counter += 1;
@@ -215,9 +219,8 @@ $(document).ready(function () {
                     compMove();
                     animate();
                 }, 1500);
-                //Checks for winner after 20 rounds
+                // Checks for winner after 20 rounds
                 if (counter === 20) {
-                    //turn off animate()
                     win = true;
                     audioBuzzer.play();
                     alert("You win!");
@@ -226,6 +229,7 @@ $(document).ready(function () {
         }
         else {
             // Strict mode ON - Any wrong player input, game restarts from round one.
+            $('.game-button').removeClass('active');
             audioBuzzer.play();
             if (strict === true) {
                 setTimeout(function () {
