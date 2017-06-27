5a6a3acdab1cc50959e87615e64450ca4b5af8a6
Drew <drewcodes@gmail.com>
2017-06-27 12:13:51 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,63 +1,29 @@
+// THIS ONE WORKS!
+
+
 $(document).ready(function(){
-  //Created variables to represent coordinates.
-  var lat;
-  var lon;
+// Geo Location Vars
+var lat;
+var lon;
 
-  //this getJSON calls for the coordinates via an API. The rest of the code executes within this call function to display the JSON object data.
-  $.getJSON('http://ip-api.com/json', function(yourLocation){
+// Get Geo Location
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
 
-    lat = yourLocation.lat;
-    lon = yourLocation.lon;
 
     //API
-    var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=1c998cd7939b1c9031ba503704852c7b';
+      var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=0d8cb05a7bab59856011d598a0126a63'+'?callback=?';
+    console.log(api);
 
-    $.getJSON(api, function(data) {
+$.getJSON(api, function(data){
+  console.log(data);
+}); // getJSON
 
-      //All variables for current weather data
-      var city = data.name;
-      var humidity = data.main.humidity;
-      var weatherType = data.weather[0].description;
-      var kTemp = data.main.temp;
-      var windSpeed = data.wind.speed;
-      var currentIcon = data.weather[0].icon;
-      var cTemp = Math.floor(kTemp-273)+' &#176;'+'c';
-      var fTemp = Math.floor(1.8 * (kTemp - 273) + 32) + ' &#176;'+'f';
 
-      var tempC = Math.floor(kTemp-273);
-      var tempF = Math.floor(1.8 * (kTemp - 273) + 32);
 
-//Capitalizing words
-function capitalizeWord(str) {
-  var convert = str.toLowerCase().split(' ');
-  var final = convert.map(function(words) {
-    return words.replace(words.charAt(0), words.charAt(0).toUpperCase());
-  });
-  return final.join(' ');
+  }); // Geo Location
 }
 
-      var tempToggle = true;
-
-      $('#city').html(city);
-      $('#tempNow').html(fTemp);
-      $('#windSpeed').html('Wind Speed: '+windSpeed+' MPH');
-      $('#weatherType').html(capitalizeWord(weatherType));
-      $('#humidity').html('Humidity: '+humidity+'%');
-
-      //Toggle
-      $("#tempToggle").click(function(){
-
-        if (tempToggle === true) {
-          $("#tempNow").html(cTemp);
-          tempToggle = false;
-        } else {
-          $('#tempNow').html(fTemp);
-          tempToggle = true;
-        }
-
-      });
-
-      
-    });
-  });
-  });
+}); // Document Ready
