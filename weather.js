8ff3417203e76fc5f527cdcde7e5abe08e3b7185
Drew <drewcodes@gmail.com>
2017-08-09 16:26:35 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -22,6 +22,7 @@ $.getJSON(api, function(data) {
       var currentIcon = data.weather[0].icon;
       var cTemp = Math.floor(kTemp-273)+' &#176;'+'c';
       var fTemp = Math.floor(1.8 * (kTemp - 273) + 32) + ' &#176;'+'f';
+
       var tempC = Math.floor(kTemp-273);
       var tempF = Math.floor(1.8 * (kTemp - 273) + 32);
 
@@ -41,19 +42,6 @@ function capitalizeWord(str) {
       $('#windSpeed').html('Wind Speed: '+windSpeed+' MPH');
       $('#weatherType').html(capitalizeWord(weatherType));
       $('#humidity').html('Humidity: '+humidity+'%');
-
-      //Background Images
-            if (fTemp >= 70) {
-              $('body').css('background-image', 'url(https://images.unsplash.com/photo-1421091242698-34f6ad7fc088?crop=entropy&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1300)')
-            } else if (fTemp < 70 && tempF >= 50) {
-              $('body').css('background-image', 'url(https://images.unsplash.com/photo-1421091242698-34f6ad7fc088?crop=entropy&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1300)')
-            } else if (fTemp < 50 && tempF >= 40) {
-              $('body').css('background-image', 'url(https://images.unsplash.com/photo-1445561696415-deadc6a2adaa?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=283323ef56689fd7d1ee25f01f113e6d)')
-            } else if (fTemp > 39) {
-              $('body').css('background-image', 'url(https://images.unsplash.com/photo-1414170562806-9d670e90c091?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=f8c6383e1ce6e90ad9713d119ab8e34f)')
-            } else {
-              $('body').css('background-image', 'url(https://images.unsplash.com/photo-1414170562806-9d670e90c091?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=f8c6383e1ce6e90ad9713d119ab8e34f)')
-            }
 
       //Toggle
       $("#tempToggle").click(function(){
